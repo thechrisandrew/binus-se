52a6ef17eca3f775d42919364031da98ecc417e0
Chris Andrew <67407803+thechrisandrew@users.noreply.github.com>
2022-03-06 21:01:34 +0700

    Revert "Register (#9)"
    This reverts commit cbb2016c696cf6205ffc179625d60c6302e67410.

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -1,14 +1,8 @@
 const express = require("express");
-
-const registerController = require("./../controllers/registerController");
 const router = express.Router();
-
-router.post("/", registerController.create_account);
-
 
 router.get("/", (req, res) => {
 	res.render("registration");
 });
-
 
 module.exports = router;
