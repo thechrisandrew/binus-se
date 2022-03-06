cbb2016c696cf6205ffc179625d60c6302e67410
JG21-1 <79459204+JoseGcarls@users.noreply.github.com>
2022-03-06 21:00:00 +0700

    Register (#9)
    * Linked register frontend and backend
    
    Co-authored-by: thechrisandrew <christopherandrew612@gmail.com>

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
+
+const registerController = require("./../controllers/registerController");
 const router = express.Router();
+
+router.post("/", registerController.create_account);
+
 
 router.get("/", (req, res) => {
 	res.render("registration");
 });
+
 
 module.exports = router;
