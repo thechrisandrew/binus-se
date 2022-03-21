4f35b20f5583ec5a3f192404ac9824a88aa79490
thechrisandrew <christopherandrew612@gmail.com>
2022-03-21 17:54:06 +0700

    code cleanup

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: "false",
-        saveUninitialized: false,
-    })
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: "false",
+		saveUninitialized: false,
+	})
 );
 
 // Application Routes
@@ -39,9 +39,9 @@ app.use("/", require("./routes/mainRoute"));
 
 // 404 Page Route
 app.use((req, res) => {
-    res.status(404).send("404 Not Found");
+	res.status(404).send("404 Not Found");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
