2bdee619031b95090852b61844f5c75f79f880f1
Chris Andrew <67407803+thechrisandrew@users.noreply.github.com>
2022-06-14 09:55:37 +0700

    General code cleanup and revision (#57)

--- public/script.js
+++
@@ -1,9 +0,0 @@
-const Login = document.querySelector("form")
-Login.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    const Valid=[...Login.querySelectorAll("input")]
-    const Check=Valid.every(item=>item.checkValidity())
-    if(Check){
-        Login.submit()
-    }
-})
